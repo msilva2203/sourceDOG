88102ce83b4ae02fe4ece76b6df8e2967e6fa95c
Marco Silva <marcosilva2203@outlook.pt>
2024-11-01 16:23:08 +0000

    Add initial source files

---
+++ src/dog.c
@@ -0,0 +1,12 @@
+/*************************************************************
+ * Project:        sourceDOG
+ * File:           dog.c
+ * Author:         Marco Manuel Almeida e Silva
+ * Created:        2024-11-01
+ *
+ * License:        MIT License (see LICENSE file in repository)
+ *
+ *************************************************************/
+
+#include "dog.h"
+
---
+++ src/dog.h
@@ -0,0 +1,16 @@
+/*************************************************************
+ * Project:        sourceDOG
+ * File:           dog.h
+ * Author:         Marco Manuel Almeida e Silva
+ * Created:        2024-11-01
+ *
+ * License:        MIT License (see LICENSE file in repository)
+ *
+ *************************************************************/
+
+#ifndef DOG_H
+#define DOG_H
+
+
+
+#endif /* DOG_H */
